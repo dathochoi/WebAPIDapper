409d26e1b59ac7b14572adce82d4d6d309a1e610
dathochoi <dathochoi01@yahoo.com.vn>
2020-01-29 21:53:28 +0700

    Add project files.

---
+++ WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly string _connectString;
+        public ProductController(IConfiguration configuration)
+        {
+            _connectString = configuration.GetConnectionString("DefaultConnection");
+        }
+        // GET: api/Product
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            using (var conn = new SqlConnection(_connectString))
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                    conn.Open();
+                var result = await conn.QueryAsync<Product>("select Id,Sku,Price,DiscountPrice,ImageUrl,CreatedAt,IsActive,ViewCount from Products", null, null, null, System.Data.CommandType.Text);
+                return result;            }
+        }
+
+        // GET: api/Product/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Product
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Product/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
